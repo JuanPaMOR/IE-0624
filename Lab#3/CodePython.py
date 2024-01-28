fc3455fe2bead89a8ea63c8fcd163173ef131602
JuanPablo <juanpablo.morales@ucr.ac.cr>
2024-01-27 23:06:23 -0600

    Codigo Python Terminado

--- Lab#3/CodePython.py
+++ Lab#3/CodePython.py
@@ -1,14 +1,28 @@
 import serial
 import time
+import csv
 
 ser = serial.Serial(port='/tmp/ttyS1',baudrate=9600)
 
 time.sleep(1)
 
+archivo = "datos.csv"
+
+titulo = ['MODO','V1','V2','V3','V4']
+
+
+with open(archivo, mode='w', newline='') as datos:
+    writer = csv.writer(datos)
+    writer.writerow(titulo)
+
+
 while True:
-    cad = ser.readline().decode('ascii')
-    print(cad)
-    print('******************')
+    Voltajes = ser.readline().decode('ascii')
+    Voltajes = Voltajes.split(',')
+    with open(archivo, mode='a', newline='') as datos:
+        writer = csv.writer(datos)
+        writer.writerow(Voltajes)
+        
 
 
 
